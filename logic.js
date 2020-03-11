8114479d2c9c2a63db59e29d28e85fc4cb16e497
dvndata <56656380+dvndata@users.noreply.github.com>
2020-03-11 01:49:49 -0400

    Map code - config index logic style findal_df_weights

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // Function to determine marker size based on salary
-function markerSize(salary) {
-  return salary / 1.2;
+function markerSizesal(salary) {
+  return salary ;
 }
 
 // Function to determine marker size based on job openings
@@ -8,13 +8,42 @@ function markerSizepos(positions) {
   return positions * 1000;
 }
 
-// Define arrays to hold created positions and salary markers
-var positionMarkers = [];
-var salaryMarkers = [];
+// Function to determine marker size based on median house prices
+function markerSizehouse(med_h) {
+  return med_h /3 ;
+}
 
-  // Create two separate layer groups: one for cities and one for states
+// Function to determine marker size based on median rents
+function markerSizemrent(med_r) {
+  return med_r * 50;
+}
+
+// Function to determine marker size based on mortgage/salary
+function markerSizemtg(mbs) {
+  return mbs * 2000;
+}
+
+// Function to determine marker size based on rent/salary
+function markerSizerent(rbs) {
+  return rbs * 2000;
+}
+
+
+// Define arrays to hold created positions and salary markers
+var salaryMarkers = [];
+var positionMarkers = [];
+var houseMarkers = [];
+var mrentMarkers = [];
+var mtgToSalMarkers = [];
+var rentToSalMarkers = [];
+
+  // Create separate layer groups
   var salary = L.layerGroup(salaryMarkers);
   var positions = L.layerGroup(positionMarkers);
+  var house = L.layerGroup(houseMarkers);
+  var mrent = L.layerGroup(mrentMarkers);
+  var mtgToSal = L.layerGroup(mtgToSalMarkers);
+  var rentToSal = L.layerGroup(rentToSalMarkers);
 
   // Define variables for our base layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -28,10 +57,20 @@ var salaryMarkers = [];
   var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5,
-    layers: [streetmap, salary, positions]
+    layers: [streetmap, salary]
   });
 
-d3.csv("output_file.csv").then(data => { 
+  // Create an overlay object
+  var overlayMaps = {
+    "Average Salary": salary,
+    "Job Openings": positions,
+    "Median home price": house,
+    "Median monthly rent": mrent,
+    "Mortgage to Salary": mtgToSal,
+    "Rent to Salary": rentToSal
+  };
+
+d3.csv("final_df_weights.csv").then(data => { 
   console.log(data);
   // Loop through cities and create salary and job opening markers
   data.forEach(cities => {
@@ -40,12 +79,12 @@ d3.csv("output_file.csv").then(data => {
       L.circle([cities.Lat, cities.Long], {
         stroke: false,
         fillOpacity: 0.75,
-        color: "green",
-        fillColor: "green",
-        radius: markerSize(cities.salary)
-      }).bindPopup("<h1>" + cities.Name + "</h1> <hr> <h3>Salary: $" + cities.salary + "</h3>").addTo(myMap)
+        color: "red",
+        fillColor: "red",
+        radius: markerSizesal(cities.salary)
+      }).bindPopup("<h1>#" + cities.rank + ": " + cities.city + "</h1> <hr> <h3>  Salary: $" + cities.salary + "</h3>").addTo(salary)
     )
-
+    
     // Setting the marker radius for the city by passing job openings into the markerSize function
     positionMarkers.push(
       L.circle([cities.Lat, cities.Long], {
@@ -54,8 +93,54 @@ d3.csv("output_file.csv").then(data => {
         color: "yellow",
         fillColor: "yellow",
         radius: markerSizepos(cities.positions)
-      }).bindPopup("<h1>" + cities.Name + "</h1> <hr> <h3>Job openings: " + cities.positions + "</h3>").addTo(myMap)
+      }).bindPopup("<h1>#" + cities.rank + ": " + cities.city + "</h1> <hr> <h3> Job openings: " + cities.positions + "</h3>").addTo(positions)
     )
+
+    // Setting the marker radius for the city by passing home prices into the markerSize function
+    houseMarkers.push(
+      L.circle([cities.Lat, cities.Long], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "blue",
+        fillColor: "blue",
+        radius: markerSizehouse(cities.med_h)
+      }).bindPopup("<h1>#" + cities.rank + ": " + cities.city + "</h1> <hr> <h3> Median Home Price: $" + cities.med_h + "</h3>").addTo(house)
+    )
+    // Setting the marker radius for the city by passing job openings into the markerSize function
+    mrentMarkers.push(
+      L.circle([cities.Lat, cities.Long], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "violet",
+        fillColor: "violet",
+        radius: markerSizemrent(cities.med_r)
+      }).bindPopup("<h1>#" + cities.rank + ": " + cities.city + "</h1> <hr> <h3> Median Rent: $" + cities.med_r + "</h3>").addTo(mrent)
+    )
+
+    // Setting the marker radius by passing mortgage/salary into the markerSize function
+    mtgToSalMarkers.push(
+      L.circle([cities.Lat, cities.Long], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "purple",
+        fillColor: "purple",
+        //radius: markerSize(1)
+        radius: markerSizemtg(cities.mbs)
+      }).bindPopup("<h1>#" + cities.rank + ": " + cities.city + "</h1> <hr> <h3>Mortgage To Salary: " + cities.mbs + "%</h3>").addTo(mtgToSal)
+    )
+
+    // Setting the marker radius by passing rent/salary into the markerSize function
+    rentToSalMarkers.push(
+      L.circle([cities.Lat, cities.Long], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "orange",
+        fillColor: "orange",
+        //radius: markerSize(2)
+        radius: markerSizerent(cities.rbs)
+      }).bindPopup("<h1>#" + cities.rank + ": " +  cities.city + "</h1> <hr> <h3>  Rent To Salary: " + cities.rbs + "%</h3>").addTo(rentToSal)
+    )
+
   })
 
   main(); 
@@ -81,7 +166,11 @@ function main() {
   // Create an overlay object
   var overlayMaps = {
     "Average Salary": salary,
-    "Job Openings": positions
+    "Job Openings": positions,
+    "House Price": house,
+    "Rent": mrent,    
+    "Mortgage to Salary": mtgToSal,
+    "Rent to Salary": rentToSal
   };
 
   // Pass our map layers into our layer control
